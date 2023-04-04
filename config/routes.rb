3248afa6efc70b496e9e18bede67ceb278fb36ff
narumifumiya <124240213+narumifumiya@users.noreply.github.com>
2023-04-04 05:23:16 +0000

    メール機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,8 @@ Rails.application.routes.draw do
   get "search" => "searches#search"
 
   resources :groups, only: [:new, :index, :show, :create, :edit, :update] do
+    get "new/mail" => "groups#new_mail"
+    get "send/mail" => "groups#send_mail"
     # group_users(中間テーブルをネストさせる)
     resource :group_users, only: [:create, :destroy]
   end
